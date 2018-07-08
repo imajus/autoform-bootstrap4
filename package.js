f99c1970bb4618ad811a02fde95793f60aa67010
Denis Perov <denis.perov@gmail.com>
2018-07-08 15:28:23 +0300

    Add imajus:common-helpers dependency

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'imajus:autoform-bootstrap4',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'Bootstrap 4 templates for Meteor AutoForm',
   // URL to the Git repository containing the source code for this package.
@@ -13,7 +13,7 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.7.0.3');
   api.use('ecmascript');
-  api.use(['underscore', 'templating', 'aldeed:autoform@6.0.0', 'aldeed:template-extension@4.0.0'], 'client');
+  api.use(['underscore', 'templating', 'aldeed:autoform@6.0.0', 'aldeed:template-extension@4.0.0', 'imajus:common-helpers@0.0.2'], 'client');
   api.mainModule('autoform-bootstrap4.js', 'client');
 });
 
