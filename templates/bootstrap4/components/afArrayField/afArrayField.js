15672cc4d8de7f828401ddf06828a742ff4d0f6c
Denis Perov <denis.perov@gmail.com>
2018-10-26 15:32:57 +0300

    Replace imajus:bootstrap-helpers "i" helpers with own one

--- templates/bootstrap4/components/afArrayField/afArrayField.js
+++ templates/bootstrap4/components/afArrayField/afArrayField.js
@@ -1,6 +1,6 @@
 import { Template } from 'meteor/templating';
 
+import '../../icon.js';
 import './afArrayField.html';
 
-Template.afArrayField_bootstrap4.helpers({
-});
+// Template.afArrayField_bootstrap4.helpers({});
