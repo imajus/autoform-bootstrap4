6ff8819f3c8f1a27cb0f51217a68f61dd7a88e92
Denis Perov <denis.perov@gmail.com>
2018-11-12 11:33:34 +0300

    Publish version 0.0.5

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'imajus:autoform-bootstrap4',
-  version: '0.0.4',
+  version: '0.0.5',
   // Brief, one-line summary of the package.
   summary: 'Bootstrap 4 templates for Meteor AutoForm',
   // URL to the Git repository containing the source code for this package.
